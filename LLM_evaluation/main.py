b23118845cc3fba6f9e35bf4ec5af0ec58aa3f32
akarim23131 <129064416+akarim23131@users.noreply.github.com>
2025-01-14 01:16:42 +0500

    Update main.py

--- LLM_evaluation/main.py
+++ LLM_evaluation/main.py
@@ -1,1 +1,17 @@
+from process_batches import process_all_batches
+
+async def main():
+    data_folder = r"Input Questions"
+    output_folder = r"Output directory"
+    models = [
+        
+        "openai/gpt-4o"
+    ]
+
+    # Process all batches and save results
+    process_all_batches(data_folder, models, output_folder)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
 
